d7e2cc03e0eb768df7edae096e4c291731cdbdf8
afkbyte <afkbyte@protonmail.com>
2023-03-17 20:19:21 -0400

    turn dev mode on

--- app/routes/api/verify.ts
+++ app/routes/api/verify.ts
@@ -12,13 +12,31 @@ if (!process.env.SISMO_GROUP_ID) {
   throw new Error('SISMO_GROUP_ID is not defined in the environment variables');
 }
 
+const devModeAddresses = {
+  "0x69420cc9b83d641470d0fea1cbf1a59d7a83df48": true,
+}
+
+
+
+const HydraS2VerifierOpts = {
+  isDevMode: true
+}
+
+const VerifierOpts = {
+  hydraS2: HydraS2VerifierOpts,
+  isDevMode: true
+}
+
 const zkConnectConfig: ZkConnectServerConfig = {
   // you will need to register an appId in the Factory
   appId: process.env.SISMO_APP_ID,
+  devMode: {
+    enabled: true, // will use the Dev Sismo Data Vault https://dev.vault-beta.sismo.io/
+  },
+  options: {
+    verifier: VerifierOpts
+  },
 }
-
-// create a new ZkConnect instance with the server configuration
-const zkConnect = ZkConnect(zkConnectConfig);
 
 const dataRequest = DataRequest({ groupId: process.env.SISMO_GROUP_ID});
 
@@ -29,6 +47,13 @@ export const action = async ({ request }: ActionArgs) => {
   }
 
   const requestData = await request.json();
+
+
+
+  // create a new ZkConnect instance with the server configuration
+  const zkConnect = ZkConnect(zkConnectConfig);
+
+  
   const zkConnectResponse = requestData.zkConnectResponse;
 
   const { vaultId, verifiedStatements } = await zkConnect.verify(
