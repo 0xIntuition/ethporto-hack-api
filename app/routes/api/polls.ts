2a705f6af055b5a3852f732324d8f88a619c7289
Christian Holman <christian@holman.codes>
2023-03-18 00:40:49 +0000

    Fixes

--- app/routes/api/polls.ts
+++ app/routes/api/polls.ts
@@ -1,34 +1,35 @@
 import { json, LoaderArgs } from "@remix-run/node";
-
+import { cors } from "remix-utils";
 
 interface Poll {
-    options: string[];
+  options: string[];
 }
 
 interface Polls {
-    [key: string]: Poll;
+  [key: string]: Poll;
 }
-  
+
 const polls: Polls = {
-    "this-is-an-awesome-slug": {
-      options: ["Option 1", "Option 2", "Option 3"],
-    },
-    "yellow-paper-evm": {
-        options: ["Option 1", "Option 2", "Option 3"],
-    },
-}
+  "this-is-an-awesome-slug": {
+    options: ["Option 1", "Option 2", "Option 3"],
+  },
+  "yellow-paper-evm": {
+    options: ["Option 1", "Option 2", "Option 3"],
+  },
+};
 
-export async function loader({ request}: LoaderArgs) {
-    if (request.method.toLowerCase() !== 'get') {
-        return json({ error: 'Invalid request method' }, { status: 400 });
+export async function loader({ request }: LoaderArgs) {
+  const requestData = await request.json();
+  const slug = requestData.slug;
+
+  return cors(
+    request,
+    (() => {
+      if (polls[slug] !== null) {
+        return json(polls[slug]);
+      } else {
+        return json({ error: "Invalid slug" }, { status: 400 });
       }
-
-  const url = new URL(request.url);
-  const slug = url.searchParams.get("slug");
-
-  if (slug && polls[slug]) {
-    return json(polls[slug]);
-  } else {
-    return json({ error: "Invalid slug" }, { status: 400 });
-  }
+    })()
+  );
 }
--- app/routes/api/verify.ts
+++ app/routes/api/verify.ts
@@ -1,31 +1,33 @@
 import { ActionArgs, json } from "@remix-run/node";
-import { ZkConnect, ZkConnectServerConfig, DataRequest } from "@sismo-core/zk-connect-server";
+import {
+  ZkConnect,
+  ZkConnectServerConfig,
+  DataRequest,
+} from "@sismo-core/zk-connect-server";
+import { cors } from "remix-utils";
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 if (!process.env.SISMO_APP_ID) {
-  throw new Error('SISMO_APP_ID is not defined in the environment variables');
+  throw new Error("SISMO_APP_ID is not defined in the environment variables");
 }
 
 if (!process.env.SISMO_GROUP_ID) {
-  throw new Error('SISMO_GROUP_ID is not defined in the environment variables');
+  throw new Error("SISMO_GROUP_ID is not defined in the environment variables");
 }
 
 const devModeAddresses = {
   "0x69420cc9b83d641470d0fea1cbf1a59d7a83df48": true,
-}
-
-
+};
 
 const HydraS2VerifierOpts = {
-  isDevMode: true
-}
+  isDevMode: true,
+};
 
 const VerifierOpts = {
   hydraS2: HydraS2VerifierOpts,
-  isDevMode: true
-}
+  isDevMode: true,
+};
 
 const zkConnectConfig: ZkConnectServerConfig = {
   // you will need to register an appId in the Factory
@@ -34,26 +36,22 @@ const zkConnectConfig: ZkConnectServerConfig = {
     enabled: true, // will use the Dev Sismo Data Vault https://dev.vault-beta.sismo.io/
   },
   options: {
-    verifier: VerifierOpts
+    verifier: VerifierOpts,
   },
-}
+};
 
-const dataRequest = DataRequest({ groupId: process.env.SISMO_GROUP_ID});
+const dataRequest = DataRequest({ groupId: process.env.SISMO_GROUP_ID });
 
+export const loader = async () => {
+  return json({ ok: true });
+};
 // Handles a post request to /api/verify
 export const action = async ({ request }: ActionArgs) => {
-  if (request.method.toLowerCase() !== 'post') {
-    return json({ error: 'Invalid request method' }, { status: 400 });
-  }
-
   const requestData = await request.json();
-
-
 
   // create a new ZkConnect instance with the server configuration
   const zkConnect = ZkConnect(zkConnectConfig);
 
-  
   const zkConnectResponse = requestData.zkConnectResponse;
 
   const { vaultId, verifiedStatements } = await zkConnect.verify(
@@ -63,8 +61,11 @@ export const action = async ({ request }: ActionArgs) => {
 
   const proofId = verifiedStatements[0].proofId;
 
-  return json({ 
-    ok: true, 
-    proofId: proofId
-  });
+  return await cors(
+    request,
+    json({
+      ok: true,
+      proofId: proofId,
+    })
+  );
 };
